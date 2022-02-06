56d6f42fd3acfcf71f86b0b1ffe5b09c9def3f61
Yu Weifeng <yu.weifeng@bcrdteam.onmicrosoft.com>
2022-02-06 21:42:10 +0800

    modify week3 homework

--- main.go
+++ main.go
@@ -5,20 +5,24 @@ import (
 	"fmt"
 	"github.com/walrusyu/gocamp007/week3"
 	"golang.org/x/sync/errgroup"
+	"time"
 )
 
 func main() {
-	stopChan := make(chan struct{}, 1)
 	g, ctx := errgroup.WithContext(context.Background())
 	g.Go(func() error {
-		server := week3.CreateServer(ctx)
-		server.Reg(":8001", &week3.MyHandler{}, stopChan)
+		server := week3.CreateServer(ctx, "server1")
+		server.Reg(":8001", &week3.MyHandler{})
 		return server.Start()
 	})
 
 	g.Go(func() error {
-		server := week3.CreateServer(ctx)
-		server.Reg(":8002", &week3.MyHandler{}, stopChan)
+		server := week3.CreateServer(ctx, "server2")
+		server.Reg(":8002", &week3.MyHandler{})
+
+		time.AfterFunc(time.Second*5, func() {
+			server.Stop()
+		})
 		return server.Start()
 	})
 	err := g.Wait()
--- week3/server.go
+++ week3/server.go
@@ -2,6 +2,7 @@ package week3
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -9,49 +10,53 @@ import (
 )
 
 type HttpServer interface {
-	Reg(string, http.Handler, <-chan struct{})
+	Reg(string, http.Handler)
 	Start() error
 	Stop() error
 }
 
 type MyServer struct {
+	name     string
 	sigChan  chan os.Signal
-	stopChan <-chan struct{}
 	server   http.Server
 	ctx      context.Context
 }
 
-func (s *MyServer) Reg(addr string, handler http.Handler, stop <-chan struct{}) {
+func (s *MyServer) Reg(addr string, handler http.Handler) {
 	s.server = http.Server{
 		Addr:    addr,
 		Handler: handler,
 	}
-	s.stopChan = stop
 }
 
 func (s *MyServer) Start() error {
+	fmt.Printf("server:%s is starting\n", s.name)
 	if s.sigChan == nil {
 		s.sigChan = make(chan os.Signal, 1)
 	}
-	signal.Notify(s.sigChan, syscall.SIGINT)
-	signal.Notify(s.sigChan, syscall.SIGHUP)
+	signal.Notify(s.sigChan, syscall.SIGINT, syscall.SIGHUP)
 	go func() {
 		select {
 		case <-s.sigChan:
-		case <-s.stopChan:
+			fmt.Printf("server:%s received signal from sigChan\n", s.name)
 		case <-s.ctx.Done():
+			fmt.Printf("server:%s is canceled\n", s.name)
 		}
 		s.Stop()
 	}()
 
+	fmt.Printf("server:%s started\n", s.name)
 	return s.server.ListenAndServe()
 }
 
 func (s *MyServer) Stop() error {
-	return s.server.Shutdown(s.ctx)
+	fmt.Printf("server:%s is stopping\n", s.name)
+	err := s.server.Shutdown(s.ctx)
+	fmt.Printf("server:%s stopped\n", s.name)
+	return err
 }
 
-func CreateServer(p context.Context) HttpServer {
+func CreateServer(p context.Context, name string) HttpServer {
 	ctx, _ := context.WithCancel(p)
-	return &MyServer{ctx: ctx}
+	return &MyServer{ctx: ctx, name: name}
 }
