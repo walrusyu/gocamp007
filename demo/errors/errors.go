afd471fed9b8cdb01f0ee2361c0e827463c74df8
Yu Weifeng <yu.weifeng@bcrdteam.onmicrosoft.com>
2022-05-02 23:09:15 +0800

    finish grpc api

---
+++ demo/errors/errors.go
@@ -0,0 +1,128 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+)
+
+const (
+	// UnknownCode is unknown code for error info.
+	UnknownCode = 500
+	// UnknownReason is unknown reason for error info.
+	UnknownReason = ""
+)
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("error: code = %d reason = %s message = %s metadata = %v", e.Code, e.Reason, e.Message, e.Metadata)
+}
+
+func toGRPCCode(grpcCode int32) codes.Code {
+	switch grpcCode {
+	case 200:
+		return codes.OK
+	case 404:
+		return codes.NotFound
+	default:
+		return codes.Internal
+	}
+}
+
+func FromGRPCCode(grpcCode codes.Code) int32 {
+	switch grpcCode {
+	case codes.OK:
+		return 200
+	case codes.NotFound:
+		return 404
+	default:
+		return 500
+	}
+}
+
+// GRPCStatus returns the Status represented by se.
+func (e *Error) GRPCStatus() *status.Status {
+	return status.New(toGRPCCode(e.Code), e.Message)
+}
+
+// Is matches each error in the chain with the target value.
+func (e *Error) Is(err error) bool {
+	if se := new(Error); errors.As(err, &se) {
+		return se.Reason == e.Reason
+	}
+	return false
+}
+
+// WithMetadata with an MD formed by the mapping of key, value.
+func (e *Error) WithMetadata(md map[string]string) *Error {
+	err := proto.Clone(e).(*Error)
+	err.Metadata = md
+	return err
+}
+
+// New returns an error object for the code, message.
+func New(code int, reason, message string) *Error {
+	return &Error{
+		Code:    int32(code),
+		Message: message,
+		Reason:  reason,
+	}
+}
+
+// Newf New(code fmt.Sprintf(format, a...))
+func Newf(code int, reason, format string, a ...interface{}) *Error {
+	return New(code, reason, fmt.Sprintf(format, a...))
+}
+
+// Errorf returns an error object for the code, message and error info.
+func Errorf(code int, reason, format string, a ...interface{}) error {
+	return New(code, reason, fmt.Sprintf(format, a...))
+}
+
+// Code returns the http code for a error.
+// It supports wrapped errors.
+func Code(err error) int {
+	if err == nil {
+		return 200 //nolint:gomnd
+	}
+	return int(FromError(err).Code)
+}
+
+// Reason returns the reason for a particular error.
+// It supports wrapped errors.
+func Reason(err error) string {
+	if err == nil {
+		return UnknownReason
+	}
+	return FromError(err).Reason
+}
+
+// FromError try to convert an error to *Error.
+// It supports wrapped errors.
+func FromError(err error) *Error {
+	if err == nil {
+		return nil
+	}
+	if se := new(Error); errors.As(err, &se) {
+		return se
+	}
+	gs, ok := status.FromError(err)
+	if ok {
+		ret := New(
+			int(FromGRPCCode(gs.Code())),
+			UnknownReason,
+			gs.Message(),
+		)
+		for _, detail := range gs.Details() {
+			switch d := detail.(type) {
+			case *errdetails.ErrorInfo:
+				ret.Reason = d.Reason
+				return ret.WithMetadata(d.Metadata)
+			}
+		}
+		return ret
+	}
+	return New(UnknownCode, UnknownReason, err.Error())
+}
---
+++ demo/examples/user_client/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"log"
+	"time"
+
+	pb "github.com/walrusyu/gocamp007/demo/api/user/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:5001", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewUserServiceClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Get(ctx, &emptypb.Empty{})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Get User: %v", r)
+
+	//var messageType2 *descriptorpb.DescriptorProto
+	var messageType *pb.User
+	fm, err := fieldmaskpb.New(messageType, "age")
+	if err == nil {
+		r, err = c.Update(ctx, &pb.UpdateRequest{
+			User: &pb.User{
+				Id:   &wrappers.Int32Value{Value: 1},
+				Name: "ywf",
+				Age:  11,
+				Address: &pb.User_Address{
+					Province: "sh",
+					City:     "hk",
+					Street:   "lc",
+				},
+			},
+			Mask: fm,
+		})
+	} else {
+		fmt.Printf("error:%v", err.Error())
+	}
+
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Update User: %v", r)
+}
